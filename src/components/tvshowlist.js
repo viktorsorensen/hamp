8a275b60562efbf270473a57655d43d63237f2b1
Viktor S <viksorensen@gmail.com>
2018-03-04 14:59:05 +0100

    initial

---
+++ src/components/tvshowlist.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TvShowList = (props) => {
+    return (
+        <div></div>
+    )
+}
+
+export default TvShowList;
---
+++ src/components/tvshows.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import TvShowList from './tvshowlist';
+
+export default class TvShows extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
